36747ec4e35e79bf38e9cd58d45afe98ee175953
Ken <kytogether@vip.qq.com>
2018-05-13 12:48:43 +0800

    Initial commit

---
+++ smartContract/love_words_wall.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var LoveWordsWallContract = function () {
+  LocalContractStorage.defineMapProperty(this, "repo", null)
+};
+
+LoveWordsWallContract.prototype = {
+  init: function () {
+    this.repo.set('records', []);
+  },
+  add: function (name, words) {
+    if (!name || !words) {
+      throw new Error('require params');
+    }
+    if (name.length > 20 || words.length > 1000) {
+      throw new Error('Length Limitation: name<20, words<1000');
+    }
+    var records = this.repo.get('records');
+    if (records.length >= 3) {
+      records.pop();
+    }
+    var record = {
+      name, words, address: Blockchain.transaction.from
+    };
+    records.unshift(record);
+    this.repo.set('records', records);
+  },
+  getRecords: function () {
+    return this.repo.get('records');
+  },
+};
+
+module.exports = LoveWordsWallContract;
